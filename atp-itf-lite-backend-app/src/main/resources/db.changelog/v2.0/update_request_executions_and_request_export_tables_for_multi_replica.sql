6987a01aadf1e7300ce77d496b8c0736ee1f135b
boba0821 <bogdan.bairamov@netcracker.com>
2025-07-23 13:05:01 +0500

    Update migration scripts v2

--- atp-itf-lite-backend-app/src/main/resources/db.changelog/v2.0/update_request_executions_and_request_export_tables_for_multi_replica.sql
+++ atp-itf-lite-backend-app/src/main/resources/db.changelog/v2.0/update_request_executions_and_request_export_tables_for_multi_replica.sql
@@ -2,15 +2,15 @@ ALTER TABLE IF EXISTS request_executions
     ADD COLUMN IF NOT EXISTS sse_id UUID,
     ADD COLUMN IF NOT EXISTS authorization_token TEXT;
 
-DO
-$$
+DO $$
     BEGIN
-        IF EXISTS (SELECT 1
-                   FROM information_schema.columns
-                   WHERE table_name = 'request_export'
-                     AND column_name = 'session_id') THEN
+        IF EXISTS (
+            SELECT 1
+            FROM information_schema.columns
+            WHERE table_name = 'request_export'
+              AND column_name = 'session_id'
+        ) THEN
             ALTER TABLE request_export
                 RENAME COLUMN session_id TO sse_id;
         END IF;
-    END
-$$;
+    END$$;
